7a43db9b3a3e495f892f4602b95cb738d73b10e0
Karthik-MP <69295691+Karthik-MP@users.noreply.github.com>
2021-06-23 21:51:27 +0530

    Updated Settings

--- AiRoot/settings.py
+++ AiRoot/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v*bhd3bh2@o9&@__0k45bh660a%u+j-&(k+a$g0hqx0b2*u^(%'
+SECRET_KEY = 'django-insecure'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
